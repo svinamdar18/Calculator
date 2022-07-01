caadf6854ae9a1d57b59b51e5c0cf58d643b7f09
Sohail <svinamdar18@gmail.com>
2022-07-01 10:58:28 +0530

    calc program

---
+++ Calc.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Calc {
+	public static void main(String args[])
+	{
+		int n1=18;
+                int n2=11;
+      
+             System.out.println("add= "+(n1+n2));
+              System.out.println("sub= "+(n1-n2));
+             System.out.println("mul= "+(n1*n2));
+              System.out.println("div= "+(n1/n2));
+ 
+ 
+	}
+	
+}
